e781d8cf7696e875f026b058127e4dfe5cf8a005
Talal Zoabi <87434615+SuperSpawn@users.noreply.github.com>
2023-09-19 17:55:27 +0300

    Fized llm-config

--- components/llm_config.py
+++ components/llm_config.py
@@ -6,7 +6,7 @@ from langchain.chat_models import ChatOpenAI
 def llm_config(session_prefix, start_key):
     llm_model_is_chat = st.checkbox(
         "Chat Model", st.session_state.get(
-            f"{session_prefix}_llm_model_is_chat", ''),
+            f"{session_prefix}_llm_model_is_chat", False),
         key=start_key)
     temperature = st.slider("Model Temperature:",
                             value=st.session_state.get(
@@ -14,7 +14,7 @@ def llm_config(session_prefix, start_key):
                             min_value=0.0, max_value=2.0, step=0.1,
                             key=start_key+1)
     model_name = st.text_input(
-        "Model Name:", value=st.session_state.get(f"{session_prefix}_llm_model_name", ''),
+        "Model Name:", value=st.session_state.get(f"{session_prefix}_llm_model_name", 'gpt-4'),
         key=start_key+2)
 
     st.session_state[f"{session_prefix}_llm_model_is_chat"] = llm_model_is_chat
