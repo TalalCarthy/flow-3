b73f0bf504f6445ff4453e591167641cec926951
Talal Zoabi <87434615+SuperSpawn@users.noreply.github.com>
2023-09-26 14:41:43 +0300

    Removed playwright, not working

--- pages/0_Test.py
+++ pages/0_Test.py
@@ -1,4 +1,3 @@
-import os
 import streamlit as st
 from langchain.chains import RetrievalQA
 from langchain.chat_models import ChatOpenAI
@@ -11,9 +10,6 @@ from langchain.document_transformers import BeautifulSoupTransformer
 
 import sys
 __import__('pysqlite3')
-
-
-os.system("playwright install")
 
 
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
